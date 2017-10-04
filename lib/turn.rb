bb354c9c1f65fd620c0de846a381c7db2e7520d4
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-04 13:53:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,7 @@ def turn(board)
   index = input_to_index(input)
     if valid_move?(board, index)
       move(board, index, token="X")
+      display_board(board)
     else
       until valid_move?(board, index)
         input = gets.strip
